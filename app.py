fcad6282d56fc144f7f3cc1197dd2ed774e5920b
Swaminathan11 <swaminathan.s@fivetran.com>
2023-11-19 22:32:13 +0530

    modified for testimg

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, this is finally compelete as I am reading from a private GCR image!!!"
+    return "Let's try to deploy via helm now!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') 
